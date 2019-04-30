2528f66ed1b364d97cce75f2ec508fcbb94bffb6
Tegan Kennedy <tegankennedy@ITProgMacFs-iMac.local>
2019-04-29 19:38:06 -0500

    commiting assignment3

--- Assignment3/Assignment3/Views/AboutPage.xaml.cs
+++ Assignment3/Assignment3/Views/AboutPage.xaml.cs
@@ -6,7 +6,7 @@ using Xamarin.Forms.Xaml;
 namespace Assignment3.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AboutPage : ContentPage
+    public partial class AboutPage
     {
         public AboutPage()
         {
