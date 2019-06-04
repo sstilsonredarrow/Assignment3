960585859388940736cc7bdc82656f1a556d59d4
Sean Stilson <smstilso@smstilso0319-mb.local>
2019-06-04 17:05:46 -0500

    Committing work stuff

---
+++ Assignment3/Assignment3.Android/RootActivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using MvvmCross.Forms.Platforms.Android.Views;
+using MvvmCross.Forms.Platforms.Android.Core;
+
+namespace Assignment3.Droid
+{
+    [Activity(Label = "Assignment3", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class RootActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<Assignment3Core.App, Assignment3.App>,
+            Assignment3Core.App, Assignment3.App>
+    {
+
+    }
+}
